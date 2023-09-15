5f1c35e9ab7824e858e611ff7083c37d7d94efea
Nico Plyley <nico@phoenixtechlab.com>
2023-09-15 07:45:49 -0700

    :sparkles: Added onError to handler errors

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import dbConnect from './utils/db';
 import todos from './blueprints/todos/routes';
-import jsonParsingMiddleware from './middleware/jsonMiddleware';
+import errorHandler from './utils/errorHandler';
 
 dbConnect()
   .then(() => console.log('DB Connected'))
@@ -10,7 +10,8 @@ dbConnect()
 
 const app = new Hono();
 app.use('*', logger());
-app.use(jsonParsingMiddleware);
 app.route('/todos', todos);
+
+app.onError(errorHandler);
 
 export default app;
