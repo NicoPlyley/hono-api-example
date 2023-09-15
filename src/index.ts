2acac73551097fa9f32aaebeb121568d074d8488
Nico Plyley <nico@phoenixtechlab.com>
2023-09-15 08:53:21 -0700

    :recycle: Refactored the error handler

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import dbConnect from './utils/db';
 import todos from './blueprints/todos/routes';
-import errorHandler from './utils/errorHandler';
+import errorHandler from './errors';
 
 (async () => {
   await dbConnect();
